e5891df8d079fd0a2b9b17915f5b9c0b34645ea3
Jon Pfeil <piffle21@gmail.com>
2014-01-05 18:49:35 -0500

    email modification

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,5 +1,5 @@
 class MessageMailer < ActionMailer::Base
-  default from: "donotreply@textbookexchange.herokuapp.com"
+  default from: "textbook@textbookexchange.herokuapp.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
