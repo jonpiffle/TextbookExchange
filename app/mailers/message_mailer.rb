2cd65e8bef279c19ffe0646bfe652b388800f708
Jon Pfeil <piffle21@gmail.com>
2014-01-05 18:22:18 -0500

    Mailer Setup

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,14 +1,16 @@
 class MessageMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "donotreply@textbookexchange.herokuapp.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.message_mailer.new_message_notification.subject
   #
-  def new_message_notification
-    @greeting = "Hi"
+  def new_message_notification(message)
+    @message = message
+    @user = message.receiver
+    @sender = message.sender
 
-    mail to: "to@example.org"
+    mail(to: @user.email, subject: 'TextbookExchange message received!')
   end
 end
